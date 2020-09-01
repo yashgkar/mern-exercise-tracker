db117718a412be77738a3645b272a7e5a17adbc9
Yash Garudkar <yashgarudkar@gmail.com>
2020-09-01 22:29:11 +0530

    Issues cleared 🚀

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,20 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
-router.route('/').get((req,res)=> {
+router.route('/').get((req, res) => {
     User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error:'+ err));
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error:' + err));
 });
 
-router.route('/add').post((res, req) => {
-    const username = req.body.username; //here's the TypeError username of undefined
-
-    const newUser = new User({username});
+router.route('/add').post((req, res) => {
+    const { username } = req.body; //here's the TypeError username of undefined
+    console.log("reaching here")
+    const newUser = new User({ username });
 
     newUser.save()
-    .then(()=> res.json('User added!'))
-    .catch(err => res.status(400).json('Error:'+ err));
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error:' + err));
 })
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const exerciesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 require('dotenv').config();
 
@@ -9,23 +11,37 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true , useCreateIndex: true});
+// mongoose.connect(uri, {useNewUrlParser: true , useCreateIndex: true});
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB database connection established successfully!');
-})
 
-const exerciesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+//@connect to mongo using this
+mongoose.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+//@the below code wasn't needed so commented it, only connect to the DB once and use that instance throughout application
+
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('MongoDB database connection established successfully!');
+// })
+
+/* @ use body parser before adding routes, so the body parses JSON first then connects the routes to 
+the application */
+
+//using body parser for not letting the body empty tried without it aswell it doesnt work
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/exercises', exerciesRouter);
 app.use('/users', usersRouter);
 
-app.use(bodyParser.urlencoded({ extended: false })) //using body parser for not letting the body empty tried without it aswell it doesnt work
-app.use(bodyParser.json())
-
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server is running on the port:' ${port}`);
 
 });
